8bbbc360bf53b581aebd9211bf1bb4bc4771c725
Jered O'Grady <jered9715@gmail.com>
2024-04-11 21:40:46 -0400

    first commit got the base controllers views and actions opened as well as fixed how home screen works

---
+++ CoffeeShopRegistration/CoffeeShopRegistration/Controllers/UserRegistrationController.cs
@@ -0,0 +1,45 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+        static List<User> _users = new List<User>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_users);
+        }
+
+        [HttpPost]
+        public IActionResult Registration(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.Id = _users.Max(x => x.Id) + 1;
+                _users.Add(user);
+
+                return RedirectToAction("Summary",user);
+            }
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult Summary(User user)
+        {
+            return View(user);
+        
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var user = _users.FirstOrDefault(x => x.Id == id);
+            return View(user);
+        
+        }
+
+    }
+}
---
+++ CoffeeShopRegistration/CoffeeShopRegistration/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopRegistration.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is needed")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is needed")]
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage ="Password is needed")]
+        public string Password { get; set; }
+
+
+
+    }
+}
